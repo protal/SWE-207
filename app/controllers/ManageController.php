d713530582545e2d32c9be6614cd1e5e5d71f00d
Harit <slipknot.opal@gmail.com>
2017-06-01 12:26:55 +0700

    test

---
+++ app/controllers/ManageController.php
@@ -0,0 +1,12 @@
+<?php
+
+class ManageController extends \Phalcon\Mvc\Controller
+{
+
+    public function indexAction()
+    {
+
+    }
+
+}
+
--- cache/_applications_xampp_xamppfiles_htdocs_swe-207_app_views_index.volt.php
+++ cache/_applications_xampp_xamppfiles_htdocs_swe-207_app_views_index.volt.php
@@ -11,23 +11,25 @@
 </head>
 
 <body>
-    <div class="container">
-        <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+
+      <?php if ($this->router->getControllerName() == 'index' || $this->router->getControllerName() == 'activity') { ?>
+        <nav class="navbar navbar-toggleable-md navbar-light" style="background-color: #c3e0f5;">
+          <div class="container">
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">SWE</a>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">หน้าแรก</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="#">กิจกรรม</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
+                        <a class="nav-link" href="#">ผู้ใช้งาน</a>
                     </li>
                 </ul>
                 <form class="form-inline my-2 my-lg-0">
@@ -35,12 +37,18 @@
                     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                 </form>
             </div>
+          </div>
         </nav>
-    </div>
+      <?php } ?>
+
     <div class="container">
         <?= $this->getContent() ?>
     </div>
-
+    <nav class="navbar fixed-bottom navbar-light bg-faded">
+      <div class="container text-right">
+        <a class="navbar-brand " href="#"><small>© 2017 - mateusmoural@hotmail.com</small></a>
+      </div>
+    </nav>
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
