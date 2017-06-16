868a834fdf679bd0a2eb1614f2be08ec8dcde8c0
SUDARAT\sp <sp.sudaratphiwon@hotmail.com>
2017-06-17 02:08:08 +0700

    ปอบอกให้อัพ

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -13,6 +13,28 @@ class AuthController extends \Phalcon\Mvc\Controller
     }
     public function checkLoginAction()
     {
-      echo "test";
+      $email = $this->request->getPost("email");
+    $password = $this->request->getPost("password");
+    $user = Users::findFirst(
+
+    [
+    "username = '" . $email . "'"
+    ]
+    );
+    if (!$user) {
+          $this->flashSession->error("invalid  login: " . $email . ",
+          Please try again");
+          return $this->response->redirect("auth/login");
     }
+
+    if ($user) {
+    if ($this->security->checkHash($password, $user->password)) {
+      $this->session->set("email", $email);
+      return $this->response->redirect("manage/index");
+    }
+    } else {
+      $this->flashSession->success("Unsuccessful login, please try again");
+      return $this->response->redirect("password/login");
+    }
+  }
 }
