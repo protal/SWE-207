631ba7803eec24a9223d438fc7190e19818ce70d
Tannnnnn <tan_sweetmulet@hotmail.com>
2017-06-21 21:17:05 +0700

    index

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -1,12 +1,24 @@
 <?php
-
+use Phalcon\Paginator\Adapter\Model as Paginator;
 class IndexController extends ControllerBase
 {
 
     public function indexAction()
     {
+      $numberPage = $this->request->getQuery("page", "int");
+      $s = $this->request->get("s");
+      $activity = Activity::query()
+                ->where("name like '%".$s."%'")
+                ->execute();
+      $paginator = new Paginator([
+        'data' => $activity,
+        'limit'=> 10,
+        'page' => $numberPage
+      ]);
 
+      $this->tag->setDefault("s", $s);
+      $this->view->page = $paginator->getPaginate();
+      $this->view->s = $s;
     }
 
 }
-
