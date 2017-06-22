e8e20c4a60885ea5748641a073735b0b3d200032
Tannnnnn <tan_sweetmulet@hotmail.com>
2017-06-22 21:45:05 +0700

    เย้ๆๆๆ

--- app/controllers/ActivityController.php
+++ app/controllers/ActivityController.php
@@ -10,20 +10,7 @@ class ActivityController extends \Phalcon\Mvc\Controller
     }
     public function showAction($id)
     {
-      $numberPage = $this->request->getQuery("page", "int");
-      $s = $this->request->get("s");
-      $activity = Activity::query()
-                ->where("name like '%".$s."%'")
-                ->orwhere("Datail like '%".$s."%'")
-                ->execute();
-      $paginator = new Paginator([
-        'data' => $activity,
-        'limit'=> 1,
-        'page' => $numberPage
-      ]);
-
-      $this->tag->setDefault("s", $s);
-      $this->view->page = $paginator->getPaginate();
-      $this->view->s = $s;
+      $activity = Activity::findFirst($id);
+      $this->view->activity = $activity;
     }
 }
