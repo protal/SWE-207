f4636ff31548edc6c1d855f00ff8f09a0bab4cc2
Tannnnnn <tan_sweetmulet@hotmail.com>
2017-06-22 21:07:49 +0700

    activity show complete 10 %

--- app/controllers/ActivityController.php
+++ app/controllers/ActivityController.php
@@ -1,4 +1,5 @@
 <?php
+use Phalcon\Paginator\Adapter\Model as Paginator;
 
 class ActivityController extends \Phalcon\Mvc\Controller
 {
@@ -7,8 +8,22 @@ class ActivityController extends \Phalcon\Mvc\Controller
     {
 
     }
-    public function showAction()
+    public function showAction($id)
     {
-      
+      $numberPage = $this->request->getQuery("page", "int");
+      $s = $this->request->get("s" = "id");
+      $activity = Activity::query()
+                ->where("name like '%".$s."%'")
+                ->orwhere("Datail like '%".$s."%'")
+                ->execute();
+      $paginator = new Paginator([
+        'data' => $activity,
+        'limit'=> 1,
+        'page' => $numberPage
+      ]);
+
+      $this->tag->setDefault("s", $s);
+      $this->view->page = $paginator->getPaginate();
+      $this->view->s = $s;
     }
 }
